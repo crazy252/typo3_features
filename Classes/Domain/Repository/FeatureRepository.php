3af3b1488faba2ebc05977d211d4b6c0bf21c128
crazy252 <crazy252.cg@gmail.com>
2024-09-28 02:21:54 +0200

    Select only first feature in repo

--- Classes/Domain/Repository/FeatureRepository.php
+++ Classes/Domain/Repository/FeatureRepository.php
@@ -2,17 +2,19 @@
 
 namespace Crazy252\Typo3Features\Domain\Repository;
 
+use Crazy252\Typo3Features\Domain\Model\Feature;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 class FeatureRepository extends Repository
 {
     /**
      * @param string $featureName
+     * @return Feature|object|null
      */
     public function getFeature($featureName)
     {
         $query = $this->createQuery();
         $query->matching($query->equals('key', $featureName));
-        return $query->execute();
+        return $query->execute()->getFirst();
     }
 }
